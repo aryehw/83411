770dea043c2feaaa94fb26fa3048616793738a6a
Aryeh <aryeh@alum.mit.edu>
2021-05-26 20:53:37 +0300

    python examples added

---
+++ scripts/python/Utils.py
@@ -0,0 +1,128 @@
+from ij import IJ,ImagePlus,WindowManager, Prefs
+from ij.io import OpenDialog, LogStream, FileSaver
+
+import os
+from os import sep, path, makedirs
+import sys, math, re, time
+from datetime import datetime
+from pprint import pprint
+
+
+def close_non_image_windows():
+	"""
+	Close all non-image windows except for the script editor, Recorder, Commander Finder, and Memory in application
+	"""
+	if WindowManager.getWindow('Results') != None:
+		WindowManager.getWindow('Results').close(False)
+	for w in WindowManager.getNonImageTitles():
+		if (w[:-3] != '.py' and w[:-4]!='.ijm' and w[:] != 'Recorder') and w[:] != "Command Finder" and w[:] != "Memory" and w[:] != "Log":
+			WindowManager.getWindow(w).close()
+
+def close_image_windows():
+	"""
+	Closes all image windows which are open in the application
+	"""
+	for w in WindowManager.getImageTitles():
+		imp = WindowManager.getImage(w)
+		imp.changes = False
+		imp.close()
+
+
+def open_image():
+	"""
+	opens an image, returns an imagePlus object and its name in that order
+	"""
+	# Prompt user for the input image file.
+	print "open_image begin"
+	op = OpenDialog("Choose input image...", "")
+	
+	if op.getPath() == None :
+		sys.exit('User canceled dialog')
+	# open selected image and prepare it for analysis  
+	
+	inputName = op.getFileName()
+	inputDirPath = op.getDirectory()
+	inputPath = inputDirPath + inputName
+
+	# Strip the suffix off of the input image name
+	if inputName[-4] == ".":
+		inputPrefix = inputName[:-4]	# assumes that a suffix exists
+	else:
+		inputPrefix = inputName
+
+	#opens image and returns it.
+	inputImp = ImagePlus(inputPath)
+
+	print "open_image finis"
+	return inputImp, inputPrefix, inputDirPath 
+
+def outputCreater(inputPath, inputPrefix, outputPrefix, noDate = False):
+	'''
+	checks that there is no directory with the name we intend to use and creates it
+	
+	input: directory name(str), root for directory creation (str), 3 constants to be used in directory name (str), (int), (int)
+	output: path to directory created (str)
+	'''
+	#get data for title of the folder
+	date = str(datetime.now().year) + str(datetime.now().month).zfill(2) + str(datetime.now().day).zfill(2)
+	loctime = str(datetime.now().hour).zfill(2) + str(datetime.now().minute).zfill(2)
+	if noDate:
+		outputDir = inputPrefix +'_'+outputPrefix+'_Output'
+	else:
+		outputDir = inputPrefix +'_'+outputPrefix+'_Output_'+date + loctime
+	outputPath = inputPath + outputDir
+
+	# create the output directory. It should never already exist,
+	# but it is good practice to check
+	# code appears to throw an exception if the directory does not exist,
+	# so this only makes a directory when this is the case
+	# the original code does not do anything to react to the results of
+	# the test, so this code changes this to request a new path from the
+	# user
+	try: 
+		if(path.exists(outputPath)):
+			return(outputPath+os.sep)
+			#this flag is only created if the path exists, in which case, until the user
+			#inputs a new valid path, the code goes into an infinite loop asking for a
+			#new valid path
+			direx = 0
+			while direx == 0:
+				warning = GenericDialog("Warning")  
+				warning.addStringField("Directory exists!!! Please input a path manually", "")
+				warning.showDialog()
+				outputPath = warning.getNextString()
+				#the contents of the if statement should not ever be used, but path.exists
+				#does what it is supposed to then the if statement deals with that
+				if warning.wasCanceled():
+					sys.exit()
+				if not(path.exists(outputPath)):
+					direx = 1
+					makedirs(outputPath)
+		else:
+			makedirs(outputPath) 
+	except:
+		makedirs(outputPath)
+	return outputPath+os.sep
+
+def getCreatedImages(inputName, nameStr):
+	# This function returns the ImagePlus, ImageProcessor, and image title associated 
+	# with an image that was open with IJ.run(something that creates an image)
+	# It was first writen for IJ.run(bioformats...), but is not restricted to bioformats
+	imp = WindowManager.getImage(inputName)
+	imp.setTitle(nameStr)
+	impTitle = imp.getTitle()
+	ip = imp.getProcessor()
+	return [imp, ip, impTitle]
+
+def saveFile(imp, format, outputDirPath):
+	fs = FileSaver(imp)
+	saveDict = {
+		'tif' : lambda: fs.saveAsTiff(outputDirPath + imp.getTitle() + ".tif"),
+		'zip' : lambda: fs.saveAsZip(outputDirPath + imp.getTitle() + ".zip"),
+		'png' : lambda: fs.saveAsPng(outputDirPath + imp.getTitle() + '.png'),
+		'txt' : lambda: fs.saveAsTxt(outputDirPath + imp.getTitle() + '.txt'),
+		'jpg' : lambda: fs.saveAsJpg(outputDirPath + imp.getTitle() + '.jpg')
+		}
+	
+	saveDict[format]()
+	return
---
+++ scripts/python/darknoise.py
@@ -0,0 +1,116 @@
+'''
+ * This script finds the best fit straight line for dark noise vs exposure time, and plots the data and the best fit
+ * line of the mean intensity of slices in a stack.
+ * 
+ * Inputs: 	1. An image stack that should contain exposures in sequence.
+ * 			2. The user is prompted for the exposures, which are input to a pop-up window. The sequence ends when a negative 
+ *			   exposure is entered. (The default entry is -1). If the number of exposures is less than 2, a default sequence
+ *			   of 10 exposures (2, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000) ms is assumed. The default sequence happens to be what I used.
+ * 			   The exposures are automatically sorted to monotonically increase.
+ * 	
+ * Outputs: 1. A plot of the data and best fit line, including the slope, offset and R squared.
+ * 			2. A table of the data values (mean intensity of each slice).
+ * 
+ * [26 May 21] converted the original ImageJ macro script to Python.
+ *			   Note that functions form Utils.py must be imported. In order for this to work,
+ *			   the path to Utils.py must be appended to teh import path (see l ine 34).
+ *		
+ * Author:			Aryeh Weiss
+ * Last modified:	26 May 2021
+ '''
+
+
+from ij import IJ
+
+import os
+from os import sys, sep, path, makedirs
+from time import sleep
+from pprint import pprint
+from ij.gui import Plot
+from ij.plugin import ZAxisProfiler
+from ij.measure import CurveFitter
+
+# The path in the next line must be changed to appropriate to the system on which this code runs
+sys.path.append(os.path.abspath("/home/amw/git/jythonIJdev/libs"))
+from Utils import *
+
+close_non_image_windows()
+close_image_windows()
+
+# If number of exposures is less than 1, assume default exposure list.
+
+exposures = []	
+while True:
+	exposures.append(IJ.getNumber("exposure (ms)", -1))
+	if exposures[-1] < 0:
+		exposures.pop()
+		exposures.sort()
+		break
+if len(exposures) < 2:
+	exposures =  [4,10,20,50,100,200,500,1000,2000,5000] # default exposures that I happen to use
+
+pprint(exposures)
+
+inputImp, inputPrefix, inputDirPath  = open_image()
+print("image directory path: ", inputDirPath.encode("ascii"))
+inputImp.setTitle(inputPrefix)
+inputTitle = inputImp.getTitle()
+print("image name: ", inputTitle.encode("ascii"))
+
+
+inputImp.setHideOverlay(True)
+inputImp.show()
+
+
+width, height, channels, slices, frames  =inputImp.getDimensions()
+print("width=", width," ; height=",  height, " ; number of channels=", channels, " ; number of slices=",  slices, " ; number of franes=  ", frames)
+
+if  slices * frames < len(exposures):
+	sys.exit("Stack size not equal to number of exposures")
+
+# remove any calibration that came with the image -- work in pixel units
+# this should be changed to allow for work in calibrated units.
+IJ.run(inputImp, "Properties...", "unit=pixel pixel_width=1 pixel_height=1 voxel_depth=1.0000000")
+# remove any selection so that the mean intensity  of the entire area is calculated.
+inputImp.killRoi()
+
+# generate a plot of the mean intensity of each image
+# this plot is never displayed. We just use the values generated 
+# by the zAxisProfiler.
+zPlot = ZAxisProfiler.getPlot(inputImp, "time")
+
+# We do not need the X values. They are just the slice number.
+# exp = zPlot.getXValues()
+levels = zPlot.getYValues()
+
+# Find the best linear fit of mean gray level vs camera exposure 
+cf = CurveFitter(exposures, list(levels))
+cf.doFit(CurveFitter.STRAIGHT_LINE)
+fitParams = cf.getParams()
+slope = fitParams[1]
+intercept = fitParams[0]
+rSqr = cf.getRSquared()
+
+print("slope=",slope," ; intercept=",intercept," ; rSquared=", rSqr)
+
+
+# Plot the data and the regression line
+newPlotFlags = Plot.TRIANGLE + Plot.X_GRID + Plot.X_NUMBERS + Plot.Y_GRID + Plot.Y_NUMBERS
+newPlot = Plot("DARK NOISE", "EXPOSURE, ms", "MEAN GRAY LEVEL",  newPlotFlags)
+newPlot.setLineWidth(2)
+newPlot.setColor("red")
+newPlot.add("triangle",  exposures, list(levels))
+newPlot.setLineWidth(1)
+newPlot.setColor("black")
+newPlot.drawLine(exposures[0], cf.f(exposures[0]),exposures[-1],cf.f(exposures[-1])) 
+newPlot.setColor("blue")
+newPlot.setFontSize(20)
+newPlot.addText("y = a+bx", 100.0, 13000.0)
+newPlot.addText("a = "+str(round(intercept,2)),100.0, 12250.0)
+newPlot.addText("b = "+str(round(slope,2)), 100.0, 11500.0)
+newPlot.addText("R squared = "+str(round(rSqr,3)), 100.0, 10750.0)
+newPlot.show()
+
+# Place the plot data into a ResultsTable
+rt = newPlot.getResultsTable()
+rt.show("Dark Noise Results")
